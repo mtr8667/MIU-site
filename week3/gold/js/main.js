ad50a1fa33e7a1551576ed8f7416205e528ffa6f
Matthew Richter <mrichter@fullsail.edu>
2012-05-22 13:18:31 -0400

    uploaded week3 test folder - all this was done trying to understand why validation wasn't working uploaded changes to week3 folder gold with correct validation changes - they are working

--- week3/gold/js/main.js
+++ week3/gold/js/main.js
@@ -2,9 +2,26 @@
 // MIU - Full Sail University
 // Project 3
 // May 17, 2012
+
+
+var parseProjectForm = function(data){
+// uses form data here 
+	console.log(data);
+};
+
 $(document).ready(function(){
 	
-	var rbform = $( "#projectform" );
-	
-	rbform.validate();
+	var 	projectform = $( "#projectform" ),
+			formerrorslink = $("#formerrorslink")
+	;
+			
+	projectform.validate({
+		invalidHandler: function(form, validator){
+			formerrorslink.click();
+		},
+		submitHandler: function(){
+			var 	data = projectform.serializeArray();
+			parseProjectForm(data);
+		}
+	});
 });
